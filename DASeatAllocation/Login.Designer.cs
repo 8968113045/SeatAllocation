fadf5316bc6cb992ea3a3da4b95f996cc6ac1532
Jaspreet <jaspreetsaini002@gmail.com>
2026-05-22 14:56:48 +0530

    changes

--- DASeatAllocation/Login.Designer.cs
+++ DASeatAllocation/Login.Designer.cs
@@ -370,7 +370,7 @@
             this.txtDBName.Name = "txtDBName";
             this.txtDBName.Size = new System.Drawing.Size(144, 21);
             this.txtDBName.TabIndex = 2;
-            this.txtDBName.Text = "PBSTECOUNS2018_Renew_2ndRound";
+            this.txtDBName.Text = "PSBTEDiploma_Main2026";
             this.txtDBName.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
             this.txtDBName.TextChanged += new System.EventHandler(this.txtDBName_TextChanged);
             // 
